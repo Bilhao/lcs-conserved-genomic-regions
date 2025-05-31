bb555a886e7918a2ae4372307874f017f427551f
simeaoversos <versosmaria@gmail.com>
2025-06-01 00:23:16 +0100

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -1,7 +1,7 @@
 class Sequence():
     def __init__(self, id: str, description: str,  seq: str):
         self.id = id
-        self.description = description
+        self.description = description  #malta tf é a description tou atoa
         self.seq = seq
 
     def length(self) -> int:
