7b9d0c3d3a52de4eb00703c6871add5b7b6fad90
Bilhão <rafaelr.bilhao@gmail.com>
2025-06-02 08:55:21 +0100

    docstrings de algumas classes e alguns métodos

--- lcs_finder.py
+++ lcs_finder.py
@@ -2,15 +2,36 @@ from sequence import Sequence
 from sequence_alignment import SequenceAlignment
 
 class LCSFinder():
+    """
+    Classe para encontrar a maior subsequência comum (LCS) entre duas ou três sequências.
+    Esta classe utiliza programação dinâmica para calcular o LCS e reconstruir as sequências alinhadas.
+    
+    Attributes:
+        seq1 (Sequence): A primeira sequência a ser comparada.
+        seq2 (Sequence): A segunda sequência a ser comparada.
+        seq3 (Sequence, optional): A terceira sequência a ser comparada. Se não for fornecida, o LCS será calculado apenas entre seq1 e seq2.
+    """
     def __init__(self, seq1: Sequence, seq2: Sequence, seq3: Sequence = None): # type: ignore
         self.seq1 = seq1
         self.seq2 = seq2
         self.seq3 = seq3
     
     def compute_lcs(self) -> SequenceAlignment:
+        """
+        Computa o alinhamento de duas ou três sequências, retornando um objeto SequenceAlignment.
+
+        Returns:
+            SequenceAlignment: Um objeto que contém informações relevantes da sequência.
+        """
         ...
     
     def get_lcs_length(self) -> int:
+        """
+        Calcula o comprimento da maior subsequência comum (LCS) entre duas ou três sequências.
+
+        Returns:
+            int: O comprimento do LCS.
+        """
         ...
     
 
--- sequence.py
+++ sequence.py
@@ -1,13 +1,39 @@
 class Sequence():
+    """
+    Classe que representa uma sequência biológicas (DNA, RNA ou proteína).
+
+    Attributes:
+        id (str): Identificador.
+        description (str): Descrição da sequência.
+        seq (str): A sequência de nucleotídeos ou aminoácidos.
+    """
     def __init__(self, id: str, description: str,  seq: str):
         self.id = id
         self.description = description  #malta tf é a description tou atoa -> é o que a sequencia é, tipo a doença que vamos escolher ou alguma outra coisa que ela representa
         self.seq = seq
 
     def length(self) -> int:
+        """
+        Calcula o comprimento da sequência.
+
+        Returns:
+            int: O comprimento da sequência.
+        """
         ...
     
     def char_at(self, position: int) -> str:
+        """
+        Retorna o caractere na posição especificada da sequência.
+
+        Parameters:
+            position (int): A posição do caractere na sequência.
+
+        Returns:
+            str: O caractere na posição especificada.
+
+        Raises:
+            IndexError: Se a posição estiver fora dos limites da sequência.
+        """
         ...
     
     def __str__(self) -> str:
